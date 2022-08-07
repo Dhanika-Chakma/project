22312f504ecf828a3ba9bcd76181b7f411cae6db
Dhanika-Chakma <110773108+Dhanika-Chakma@users.noreply.github.com>
2022-08-07 18:54:55 +0530

    Add files via upload

---
+++ randomcharcter.py
@@ -0,0 +1,34 @@
+# a=["roshni","dhanika","swari"]
+# b.split()
+# print(b)
+# a=int(input("enter the sum"))
+# b=int(input("enter the sum"))
+# c=int(input("enter the sum"))
+# print(a+b+c)
+
+
+
+# a=int(input("enter muoltipication"))
+# b=int(input("enter multipication"))
+# print(f"my outpot{a}")
+# print(f"my outpot{b}")
+
+# # there are 5280 feet in a mile. Write a Python statement that 
+# calculates and prints the number of feet in 13 miles
+
+# print ("No. of feet in 13 miles =",5280*13,
+# feet=int(input("enter feed no  :"))
+# mile=int(input("enter mile   :"))
+# print("feet:",5280*13,"in 13 miles")
+     
+     
+# Write a Python statement that calculates and prints the number of 
+# seconds in 7 hours, 21 minutes and 37 seconds.
+# hour=7
+# minute=21
+# second=37
+# print(hour*60*60+minute*60+second,'second')
+length=4
+height=7
+perimeter=2*(length+height)
+print(perimeter)
